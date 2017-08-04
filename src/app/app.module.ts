b799799228971eee97e7a2dcf53b936e8d1b9810
Thiago Passos <thiago@passos.com.au>
2017-08-05 00:32:00 +1000

    updating navigation and adding components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,15 +7,27 @@ import { AppComponent } from './components/app/app.component';
 import { NavMenuComponent } from './components/navmenu/navmenu.component';
 import { HomeComponent } from './components/home/home.component';
 import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
-import { CounterComponent } from './components/counter/counter.component';
+import { CategoryComponent } from './components/category/category.component';
+import { SupplierComponent } from './components/supplier/supplier.component';
+import { ProductComponent } from './components/product/product.component';
+import { CustomerComponent } from './components/customer/customer.component';
+import { OrderComponent } from './components/order/order.component';
+import { EmployeeComponent } from './components/employee/employee.component';
+import { SettingComponent } from './components/setting/setting.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
-    CounterComponent,
     FetchDataComponent,
-    HomeComponent
+    HomeComponent,
+    CategoryComponent,
+    SupplierComponent,
+    ProductComponent,
+    CustomerComponent,
+    OrderComponent,
+    EmployeeComponent,
+    SettingComponent,
   ],
   imports: [
     BrowserModule,
@@ -23,8 +35,14 @@ import { CounterComponent } from './components/counter/counter.component';
     RouterModule.forRoot([
       { path: '', redirectTo: 'home', pathMatch: 'full' },
       { path: 'home', component: HomeComponent },
-      { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
+      { path: 'category', component: CategoryComponent },
+      { path: 'supplier', component: SupplierComponent },
+      { path: 'product', component: ProductComponent },
+      { path: 'customer', component: CustomerComponent },
+      { path: 'order', component: OrderComponent },
+      { path: 'employee', component: EmployeeComponent },
+      { path: 'setting', component: SettingComponent },
       { path: '**', redirectTo: 'home' }
     ])
   ],
---
+++ src/app/components/category/category.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-category',
+  templateUrl: './category.component.html',
+  styleUrls: ['./category.component.css']
+})
+export class CategoryComponent implements OnInit {
+  categories = [];
+  constructor() { }
+
+  ngOnInit() {
+    this.categories = [
+      { id: 1, name: 'A' },
+      { id: 2, name: 'B' },
+      { id: 3, name: 'C' },
+      { id: 4, name: 'D' },
+      { id: 5, name: 'E' },
+      { id: 6, name: 'F' },
+      { id: 7, name: 'G' },
+    ]
+  }
+
+}
